27a604d931a959651c3e87e5e244c8f0f41951f2
taejin_noh <taejin_noh@tmax.co.kr>
2020-10-18 16:09:42 +0900

    express의 middle 사용

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../lib/template.js');
+
+router.get('/', function (request, response) {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(
+        title,
+        list,
+        `<h2>${title}</h2>${description}<img src='/images/hello.jpg' style='width:300px; top-margin:5px; display:block;'>`,
+        `<a href="/topic/create">create</a>`
+    );
+    response.send(html);
+});
+
+module.exports = router;
